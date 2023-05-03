5c1276f2c59ece97ea2a3e47ea0a47c36d607294
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-05-03 13:33:02 -0600

    agg método 'btnSignUp' para iniciar la actividad 'MainActivity'

--- app/src/main/java/com/zerocode/login_signup_ui/LoginActivity.java
+++ app/src/main/java/com/zerocode/login_signup_ui/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.zerocode.login_signup_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void btnSignUp(View view) {
+
+        Intent intentLogin = new Intent(this, MainActivity.class);
+        startActivity(intentLogin);
+
     }
 }
