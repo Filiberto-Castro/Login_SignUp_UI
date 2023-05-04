f006999030974d2a5c608e66f629f59d52a0351b
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-05-04 16:22:46 -0600

    Mejorando la logica del SignUp en LoginActivity.java

--- app/src/main/java/com/zerocode/login_signup_ui/LoginActivity.java
+++ app/src/main/java/com/zerocode/login_signup_ui/LoginActivity.java
@@ -5,19 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private CheckBox cbTerms;
+    public static final String EXTRA_EMAIL = "com.zerocode.login_signup_ui.extra.EMAIL";
+    public static final String EXTRA_PASSWORD = "com.zerocode.login_signup_ui.extra.PASSWORD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etEmail = findViewById(R.id.editText_email);
+        etPassword = findViewById(R.id.editText_password);
+        cbTerms = findViewById(R.id.checkBox);
     }
 
     public void btnSignUp(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
 
-        Intent intentLogin = new Intent(this, MainActivity.class);
-        startActivity(intentLogin);
-
+        if (!email.isEmpty() && !password.isEmpty() && cbTerms.isChecked()){
+            Intent intentLogin = new Intent(this, MainActivity.class);
+            intentLogin.putExtra(EXTRA_EMAIL, email);
+            intentLogin.putExtra(EXTRA_PASSWORD, password);
+            startActivity(intentLogin);
+        }else{
+            Toast.makeText(this, "Complete the information", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/zerocode/login_signup_ui/MainActivity.java
+++ app/src/main/java/com/zerocode/login_signup_ui/MainActivity.java
@@ -2,13 +2,24 @@ package com.zerocode.login_signup_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvTitle;
+    private TextView tvSubtitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTitle = findViewById(R.id.text_title);
+        tvSubtitle = findViewById(R.id.text_subtitle);
+        Intent intentExtra = getIntent();
+        String email = intentExtra.getStringExtra(LoginActivity.EXTRA_EMAIL);
+        String password = intentExtra.getStringExtra(LoginActivity.EXTRA_PASSWORD);
+        tvTitle.setText(email);
+        tvSubtitle.setText(password);
     }
 }
